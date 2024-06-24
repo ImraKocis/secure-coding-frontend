ce74058db93b6e64914c57bcfbad908a5f5332d0
Imra Kocis <imrakocis@gmail.com>
2024-06-25 00:44:33 +0200

    changes for burp

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
-const securityHeaders = [
-    {
-        key: 'X-Frame-Options',
-        value: 'SAMEORIGIN'
-    },
-]
-const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: securityHeaders,
-            },
-        ]
-    }
-};
+
+const nextConfig = {};
 
 // BURP: remove header to trigger risk
 // BURP: add middleware.js to trigger risk (https://github.com/vercel/next.js/blob/canary/examples/with-strict-csp/middleware.js)
